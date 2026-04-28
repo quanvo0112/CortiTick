19a1e1638ee1bd12143e8f10bb434b6d93ca2e55
quanvo0112 <quanhoconline0112@gmail.com>
2026-04-28 16:17:29 +0700

    Remove .next folder from git tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qBaT3XXonG7CG3BhTGJjU/b5TppbI/Tj3bTNi1vJ/U0=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_0.s4ckf._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_07xii7t._.js"],
-    source: "dynamic"
-});
